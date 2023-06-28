5af665afda8c8bb6e5084136d1b28bf9896b27c8
933yee <kevins30102@yahoo.com.tw>
2023-06-28 19:40:01 +0800

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useRef } from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Input
+} from 'reactstrap';
+import './Main.css';
+
+function Main() {
+    return (
+        <div className='main'>
+            Test
+        </div>
+    );
+};
+
+export default connect;
---
+++ src/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
+
+import Main from 'components/Main.jsx';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+const loggerMiddleware = createLogger();
+
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    // combineReducers({
+    // }),
+    // for debugging
+    // composeEnhancers(applyMiddleware(thunkMiddleware, loggerMiddleware))
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Main />,
+    </Provider>,
+    document.getElementById('root')
+);
