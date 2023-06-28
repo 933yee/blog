8ad08aca40d9ea793c1ad1de0e33005a9a4dc3df
933yee <kevins30102@yahoo.com.tw>
2023-06-28 22:39:36 +0800

    complete 70% basic Main interface

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -14,14 +14,78 @@ import {
     NavLink,
     Input
 } from 'reactstrap';
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import './Main.css';
 
 function Main() {
+    // window.location.href = '/';
+
+    const handleClickHomepage = () => {
+        window.location.href = '/';
+    }
+
     return (
         <div className='main'>
-            Test
+            <div className='leftbar'>
+                <div className='introduce'>
+                    <img src='./image/avatar.png' className='avatar'
+                        onClick={handleClickHomepage}>
+                    </img>
+                    <div className='name'
+                        onClick={handleClickHomepage}>
+                        Kevin's blog
+                    </div>
+                    <div className='motto'>
+                        sip, code, espresso yourself.
+                    </div>
+                </div>
+                <Router>
+                    <div className='options'>
+                        <Link to="/" className='item'>
+                            home
+                        </Link>
+                        <Link to="/categories" className='item'>
+                            categories
+                        </Link>
+                        <Link to="/tags" className='item'>
+                            tags
+                        </Link>
+                        <Link to="/archives" className='item'>
+                            archives
+                        </Link>
+                        <Link to="/about" className='item'>
+                            about
+                        </Link>
+                    </div>
+                </Router>
+                <div className='contact'>
+                    <a href='https://www.facebook.com/profile.php?id=100003747192616' className='contact-item'>
+                        <i className="fa-brands fa-facebook"></i>
+                    </a>
+                    <a href='https://github.com/933yee' className='contact-item'>
+                        <i className="fa-brands fa-github"></i>
+                    </a>
+                    <a href='mailto:kevins30102@yahoo.com' className='contact-item'>
+                        <i className="fa-solid fa-envelope"></i>
+                    </a>
+                    <a href='https://www.youtube.com/channel/UCSbMujwFekgRcjlzJcKIj8g' className='contact-item'>
+                        <i className="fa-brands fa-youtube"></i>
+                    </a>
+                    <a href='https://github.com/933yee/blog' className='contact-item'>
+                        <i className="fa-brands fa-sourcetree"></i>
+                    </a>
+                </div>
+            </div>
+            <div className='right'>
+                <div className='navbar'>
+                    yee
+                </div>
+                <div className='contents'>
+
+                </div>
+            </div>
         </div>
     );
 };
 
-export default connect;
+export default Main;
--- src/index.jsx
+++ src/index.jsx
@@ -8,22 +8,23 @@ import { createLogger } from 'redux-logger';
 import Main from 'components/Main.jsx';
 
 import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 
 const loggerMiddleware = createLogger();
 
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-    // combineReducers({
-    // }),
-    // for debugging
-    // composeEnhancers(applyMiddleware(thunkMiddleware, loggerMiddleware))
-);
+// const store = createStore(
+// combineReducers({
+// }),
+// for debugging
+// composeEnhancers(applyMiddleware(thunkMiddleware, loggerMiddleware))
+// );
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Main />,
-    </Provider>,
+    // <Provider store={store}>
+    <Main />,
+    // </Provider>,
     document.getElementById('root')
 );
